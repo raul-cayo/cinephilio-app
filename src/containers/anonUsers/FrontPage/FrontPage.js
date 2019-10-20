6908bffec3326772d7a11c76cad0d01fbac38fb4
Raul Sanchez Cayo <raul.sanchez.cayo@gmail.com>
2019-10-20 18:15:05 -0500

    Add css to FrontPage

--- src/containers/anonUsers/FrontPage/FrontPage.js
+++ src/containers/anonUsers/FrontPage/FrontPage.js
@@ -2,15 +2,16 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 import Logo from '../../../images/LogoDark.png';
+import './FrontPage.css';
 
 function FrontPage(props) {
   return (
       <div className="container mt-5">
           <div className="row align-self-center">
-              <img className="main-logo rounded-circle mx-auto d-block" src={Logo} alt="Logo Cinephilio"/>
+              <img className="FrontPage-logo rounded-circle mx-auto d-block" src={Logo} alt="Logo Cinephilio"/>
           </div>
           <div className="row align-self-center">
-              <h1 className="main-title my-3 text-center mx-auto">Cinephilio</h1>
+              <h1 className="FrontPage-title my-3 text-center mx-auto">Cinephilio</h1>
           </div>
           <div className="row align-self-center px-4">
               <Link to="/login" className="btn cbt-lg-green btn-block mb-3 col-lg-4 offset-lg-4">Iniciar Sesión</Link>
