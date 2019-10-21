e4e66400c37aef8885ec26ddb913acfa933f9a6e
Raul Sanchez Cayo <raul.sanchez.cayo@gmail.com>
2019-10-20 21:25:08 -0500

    Format code

--- src/containers/anonUsers/About/About.js
+++ src/containers/anonUsers/About/About.js
@@ -5,36 +5,36 @@ import PicCayo from '../../../images/cayo.jpg'
 import PicDiego from '../../../images/diego.jpg'
 
 function About(props) {
-  return(
+  return (
     <section className="team-section text-center m-5">
-    <p className="grey-text w-responsive mx-auto mb-5">Conoce al equipo de desarrollo</p>
+      <p className="grey-text w-responsive mx-auto mb-5">Conoce al equipo de desarrollo</p>
 
-    <div className="row">
-      <div className="col-lg-4 col-md-5 mb-lg-0 mb-5">
-        <div className="avatar mx-auto">
-          <img src={PicCayo} className="img-fluid img-thumbnail rounded-circle z-depth-1"
-            alt="Sample avatar"/>
+      <div className="row">
+        <div className="col-lg-4 col-md-5 mb-lg-0 mb-5">
+          <div className="avatar mx-auto">
+            <img src={PicCayo} className="img-fluid img-thumbnail rounded-circle z-depth-1"
+              alt="Sample avatar" />
+          </div>
+          <h5 className="font-weight-bold mt-4 mb-3">Raúl Cayo</h5>
+          <p className="text-uppercase blue-text"><strong>API developer</strong></p>
+          <p className="grey-text">Estudiante Ing. Informática.</p>
         </div>
-        <h5 className="font-weight-bold mt-4 mb-3">Raúl Cayo</h5>
-        <p className="text-uppercase blue-text"><strong>API developer</strong></p>
-        <p className="grey-text">Estudiante Ing. Informática.</p>
-      </div>
 
-      <div className="col-lg-4 col-md-5 mb-lg-0 mb-5">
-        <div className="avatar mx-auto">
-          <img src={PicLuis} className="img-fluid img-thumbnail rounded-circle z-depth-1"
-            alt="Sample avatar"/>
+        <div className="col-lg-4 col-md-5 mb-lg-0 mb-5">
+          <div className="avatar mx-auto">
+            <img src={PicLuis} className="img-fluid img-thumbnail rounded-circle z-depth-1"
+              alt="Sample avatar" />
+          </div>
+          <h5 className="font-weight-bold mt-4 mb-3">Luis Hernández</h5>
+          <p className="text-uppercase blue-text"><strong>Front end developer</strong></p>
+          <p className="grey-text">Estudiante Ing. Informática.</p>
         </div>
-        <h5 className="font-weight-bold mt-4 mb-3">Luis Hernández</h5>
-        <p className="text-uppercase blue-text"><strong>Front end developer</strong></p>
-        <p className="grey-text">Estudiante Ing. Informática.</p>
-      </div>
 
-      <div className="col-lg-4 col-md-10 mb-md-0 mb-5">
-        <div className="avatar mx-auto">
-          <img src={PicDiego} className="img-fluid img-thumbnail rounded-circle z-depth-1"
-            alt="Sample avatar"/>
-        </div>
+        <div className="col-lg-4 col-md-10 mb-md-0 mb-5">
+          <div className="avatar mx-auto">
+            <img src={PicDiego} className="img-fluid img-thumbnail rounded-circle z-depth-1"
+              alt="Sample avatar" />
+          </div>
           <h5 className="font-weight-bold mt-4 mb-3">Diego Ramírez</h5>
           <p className="text-uppercase blue-text"><strong>DBA</strong></p>
           <p className="grey-text">Estudiante Ing. en Computación.</p>
--- src/containers/anonUsers/FrontPage/FrontPage.js
+++ src/containers/anonUsers/FrontPage/FrontPage.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import Logo from '../../../images/LogoDark.png';
 import './FrontPage.css';
 
 function FrontPage(props) {
   return (
-      <div className="container mt-5">
-          <div className="row align-self-center">
-              <img className="FrontPage-logo rounded-circle mx-auto d-block" src={Logo} alt="Logo Cinephilio"/>
-          </div>
-          <div className="row align-self-center">
-              <h1 className="FrontPage-title my-3 text-center mx-auto">Cinephilio</h1>
-          </div>
-          <div className="row align-self-center px-4">
-              <Link to="/login" className="btn cbt-lg-green btn-block mb-3 col-lg-4 offset-lg-4">Iniciar Sesión</Link>
-              <Link to="/signup" className="btn cbt-lg-green btn-block mb-3 col-lg-4 offset-lg-4">Registrarme</Link>
-              <Link to="/about" className="btn cbt-blue btn-block mb-3 col-lg-4 offset-lg-4">¿Qué es Cinephilio?</Link>
-              <Link to="/anonRecomm" className="btn cbt-blue btn-block mb-3 col-lg-4 offset-lg-4">Recomiéndame una Película</Link>
-          </div>
+    <div className="container mt-5">
+      <div className="row align-self-center">
+        <img className="FrontPage-logo rounded-circle mx-auto d-block" src={Logo} alt="Logo Cinephilio" />
       </div>
+      <div className="row align-self-center">
+        <h1 className="FrontPage-title my-3 text-center mx-auto">Cinephilio</h1>
+      </div>
+      <div className="row align-self-center px-4">
+        <Link to="/login" className="btn cbt-lg-green btn-block mb-3 col-lg-4 offset-lg-4">Iniciar Sesión</Link>
+        <Link to="/signup" className="btn cbt-lg-green btn-block mb-3 col-lg-4 offset-lg-4">Registrarme</Link>
+        <Link to="/about" className="btn cbt-blue btn-block mb-3 col-lg-4 offset-lg-4">¿Qué es Cinephilio?</Link>
+        <Link to="/anonRecomm" className="btn cbt-blue btn-block mb-3 col-lg-4 offset-lg-4">Recomiéndame una Película</Link>
+      </div>
+    </div>
   )
 }
 
