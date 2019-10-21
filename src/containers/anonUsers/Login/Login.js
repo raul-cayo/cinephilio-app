0c54657e35cbf351ea69ba2fe0b68cec74e5210a
Raul Sanchez Cayo <raul.sanchez.cayo@gmail.com>
2019-10-20 19:11:23 -0500

    Add css to Login

--- src/containers/anonUsers/Login/Login.js
+++ src/containers/anonUsers/Login/Login.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 import Logo from '../../../images/LogoDark.png';
+import './Login.css';
 
 class Login extends React.Component {
   constructor(props){
