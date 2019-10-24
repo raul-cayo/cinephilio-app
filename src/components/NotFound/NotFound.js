6bb4caa7df938ebc9783d659ef3a9d06c601ac99
Raul Sanchez Cayo <raul.sanchez.cayo@gmail.com>
2019-10-24 16:42:09 -0500

    Add container to not found page

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -4,11 +4,12 @@ import PicError from '../../../images/error.jpg';
 
 function NotFound(props) {
   return (
-    <h1>404: Pagina no encontrada</h1>
-    <div class="col-8">
-      <img className="d-block mx-auto my-2" src={PicError} alt="Logo Cinephilio" />
+    <div>
+      <h1>404: Pagina no encontrada</h1>
+      <div class="col-8">
+        <img className="d-block mx-auto my-2" src={PicError} alt="Logo Cinephilio" />
+      </div>
     </div>
-      
   )
 }
 
