42e8ffaf42ffccdb7423a8102704ea472664fa8d
Raul Sanchez Cayo <raul.sanchez.cayo@gmail.com>
2019-10-23 23:32:33 -0500

    Remove elements from navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -8,28 +8,24 @@ class Navbar extends React.Component {
   render() {
     return (
       <nav className="cphio-navbar navbar navbar-light">
-        <div className="container-fluid">
 
-          <div className="navbar-header">
-            <Link to="/home" className="titulo-navbar px-2">Cinephilio</Link>
-          </div>
+        <Link to="/home" className="titulo-navbar px-2">Cinephilio</Link>
 
-          <p className="my-auto ml-auto mr-3 d-none d-sm-block">{this.props.username}</p>
+        <p className="my-auto ml-auto mr-3 d-none d-sm-block">{this.props.username}</p>
 
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-            <span className="navbar-toggler-icon"></span>
-          </button>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-          <div className="collapse navbar-collapse" id="collapsibleNavbar">
-            <ul className="navbar-nav">
-              <li className="nav-item text-center mb-1 mt-2">
-                <button className="nav-btn">Mi Cuenta</button>
-              </li>
-              <li className="nav-item text-center">
-                <button className="nav-btn">Cerrar Sesión</button>
-              </li>
-            </ul>
-          </div>
+        <div className="collapse navbar-collapse" id="collapsibleNavbar">
+          <ul className="navbar-nav">
+            <li className="nav-item text-center mb-1 mt-2">
+              <button className="nav-btn">Mi Cuenta</button>
+            </li>
+            <li className="nav-item text-center">
+              <button className="nav-btn">Cerrar Sesión</button>
+            </li>
+          </ul>
         </div>
       </nav>
     )
