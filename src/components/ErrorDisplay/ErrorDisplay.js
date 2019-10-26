6c48696e0fe6155321b63bcab71fe3ffab788a59
Raul Sanchez Cayo <raul.sanchez.cayo@gmail.com>
2019-10-26 17:59:25 -0500

    Add button and set flex-column

--- src/components/ErrorDisplay/ErrorDisplay.js
+++ src/components/ErrorDisplay/ErrorDisplay.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import PicError from '../../images/error.jpg';
 import './ErrorDisplay.css';
 
 function ErrorDisplay(props) {
   return (
-    <div class="d-flex justify-content-center">
+    <div class="d-flex justify-content-center flex-column">
       <h1 className="mt-3">{props.error}</h1>
       <p>{props.desc}</p>
-      <div class="col-4">
-        <img className="d-block mx-auto" src={PicError} alt="Imagen Error" />
+      <div class="col-6 col-md-4">
+        <img className="d-block mx-auto mb-2" src={PicError} alt="Imagen Error" />
+        <Link to="/" className="btn cbt-blue btn-block">Ir a Página Principal</Link>
       </div>
     </div>
   )
