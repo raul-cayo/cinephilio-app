453ea0404a0ade08292844f261a75a96689e339c
Raul Sanchez Cayo <raul.sanchez.cayo@gmail.com>
2019-10-26 18:15:00 -0500

    Add margin between button and img

--- src/components/ErrorDisplay/ErrorDisplay.js
+++ src/components/ErrorDisplay/ErrorDisplay.js
@@ -9,9 +9,9 @@ function ErrorDisplay(props) {
     <div class="d-flex justify-content-center flex-column">
       <h1 className="mt-3 text-center">{props.error}</h1>
       <p className="text-center">{props.desc}</p>
-      <div class="col-6 offset-3 col-lg-4 offset-lg-4">
+      <div class="col-6 offset-3">
         <img className="img-fluid" src={PicError} alt="Imagen Error" />
-        <Link to="/" className="btn cbt-blue btn-block">Ir a Página Principal</Link>
+        <Link to="/" className="btn cbt-blue btn-block mt-2">Ir a Página Principal</Link>
       </div>
     </div>
   )
