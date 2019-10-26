adf49ccd4c61942c307b1eee08f564f234bbc0e4
Raul Sanchez Cayo <raul.sanchez.cayo@gmail.com>
2019-10-26 18:08:54 -0500

    Imporve erro display style

--- src/components/ErrorDisplay/ErrorDisplay.js
+++ src/components/ErrorDisplay/ErrorDisplay.js
@@ -7,10 +7,10 @@ import './ErrorDisplay.css';
 function ErrorDisplay(props) {
   return (
     <div class="d-flex justify-content-center flex-column">
-      <h1 className="mt-3">{props.error}</h1>
-      <p>{props.desc}</p>
-      <div class="col-6 col-md-4">
-        <img className="d-block mx-auto mb-2" src={PicError} alt="Imagen Error" />
+      <h1 className="mt-3 text-center">{props.error}</h1>
+      <p className="text-center">{props.desc}</p>
+      <div class="col-6 offset-3 col-lg-4 offset-lg-4">
+        <img className="img-fluid" src={PicError} alt="Imagen Error" />
         <Link to="/" className="btn cbt-blue btn-block">Ir a Página Principal</Link>
       </div>
     </div>
