a603797f61fb8b88f9d124343041195530d9e0d6
Raul Sanchez Cayo <raul.sanchez.cayo@gmail.com>
2019-10-27 21:18:41 -0600

    About...

--- src/containers/anonUsers/About/About.js
+++ src/containers/anonUsers/About/About.js
@@ -6,13 +6,13 @@ import PicDiego from '../../../images/diego.jpg'
 
 function About(props) {
   return (
-    <section className="m-5 col-12 col-lg-8 col-offset-lg-2">
+    <section className="m-5 col-12 col-lg-8 offset-lg-2">
       <h1 className="text-center mb-2">¿Qué es Cinephilio?</h1>
       <p>Es un motor de recomendaciones que se basa en las respuestas a las encuestas que realiza cada usuario para recomendarle la mejor película. Para decidir que película se va a recomendar se utilizan algoritmos y métodos de Machine Learning.</p>
 
       <h3 className="mt-5 mb-3"><strong>Equipo de Desarrollo</strong></h3>
       <div className="row">
-        <div className="col-12 col-sm-4 px-5 px-sm-3 px-md-5">
+        <div className="col-12 col-sm-4 px-5 px-sm-3">
           <div className="avatar mx-auto mb-2">
             <img src={PicCayo} className="img-fluid img-thumbnail rounded-circle" />
           </div>
@@ -20,7 +20,7 @@ function About(props) {
           <p className="text-center mt-0"><strong>API DEVELOPER</strong></p>
         </div>
 
-        <div className="col-12 col-sm-4 px-5 px-sm-3 px-md-5">
+        <div className="col-12 col-sm-4 px-5 px-sm-3">
           <div className="avatar mx-auto mb-2">
             <img src={PicLuis} className="img-fluid img-thumbnail rounded-circle" />
           </div>
@@ -28,7 +28,7 @@ function About(props) {
           <p className="text-center mt-0"><strong>FRONT END DEVELOPER</strong></p>
         </div>
 
-        <div className="col-12 col-sm-4 px-5 px-sm-3 px-md-5">
+        <div className="col-12 col-sm-4 px-5 px-sm-3">
           <div className="avatar mx-auto mb-2">
             <img src={PicDiego} className="img-fluid img-thumbnail rounded-circle" />
           </div>
