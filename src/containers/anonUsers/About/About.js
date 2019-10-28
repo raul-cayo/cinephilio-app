ccc446631e0941a1656b10205450ca9aab21cd01
Raul Sanchez Cayo <raul.sanchez.cayo@gmail.com>
2019-10-27 21:04:16 -0600

    Even more responsive

--- src/containers/anonUsers/About/About.js
+++ src/containers/anonUsers/About/About.js
@@ -10,9 +10,9 @@ function About(props) {
       <h1 className="text-center mb-2">¿Qué es Cinephilio?</h1>
       <p>Es un motor de recomendaciones que se basa en las respuestas a las encuestas que realiza cada usuario para recomendarle la mejor película. Para decidir que película se va a recomendar se utilizan algoritmos y métodos de Machine Learning.</p>
 
-      <h3 className="mt-5 mb-2"><strong>Equipo de Desarrollo</strong></h3>
+      <h3 className="mt-5 mb-3"><strong>Equipo de Desarrollo</strong></h3>
       <div className="row">
-        <div className="col-12 col-md-4 px-1 px-sm-2 px-md-5">
+        <div className="col-12 col-sm-4 px-3 px-md-5">
           <div className="avatar mx-auto mb-2">
             <img src={PicCayo} className="img-fluid img-thumbnail rounded-circle" />
           </div>
@@ -20,7 +20,7 @@ function About(props) {
           <p className="text-center mt-0"><strong>API DEVELOPER</strong></p>
         </div>
 
-        <div className="col-12 col-md-4 px-1 px-sm-2 px-md-5">
+        <div className="col-12 col-sm-4 px-3 px-md-5">
           <div className="avatar mx-auto mb-2">
             <img src={PicLuis} className="img-fluid img-thumbnail rounded-circle" />
           </div>
@@ -28,7 +28,7 @@ function About(props) {
           <p className="text-center mt-0"><strong>FRONT END DEVELOPER</strong></p>
         </div>
 
-        <div className="col-12 col-md-4 px-1 px-sm-2 px-md-5">
+        <div className="col-12 col-sm-4 px-3 px-md-5">
           <div className="avatar mx-auto mb-2">
             <img src={PicDiego} className="img-fluid img-thumbnail rounded-circle" />
           </div>
