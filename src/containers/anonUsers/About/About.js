63d6bfb4c2e26aaf05d34173ed441b287df6f17f
Raul Sanchez Cayo <raul.sanchez.cayo@gmail.com>
2020-01-13 20:36:01 -0600

    Pictures on about say UDG

--- src/containers/anonUsers/About/About.js
+++ src/containers/anonUsers/About/About.js
@@ -5,6 +5,7 @@ import SpeechBalloon from '../../../components/SpeechBalloon/SpeechBalloon';
 import PicLuis from '../../../images/developers/luis.jpg';
 import PicCayo from '../../../images/developers/cayo.jpg';
 import PicDiego from '../../../images/developers/diego.jpg';
+import './About.css';
 
 function About(props) {
   return (
@@ -17,10 +18,10 @@ function About(props) {
             Para decidir que película se va a recomendar se utilizan algoritmos 
             y métodos de Machine Learning.</SpeechBalloon>
 
-          <div className="row mt-5">
+          <div className="udg row mt-5">
             <div className="col-8 offset-2 offset-sm-0 col-sm-4 px-5 px-sm-3">
               <div className="avatar mx-auto mb-2">
-                <img alt="Cayo" src={PicCayo} className="img-fluid img-thumbnail rounded-circle" />
+                <img alt="Cayo" src={PicCayo} className="letter letterU img-fluid" />
               </div>
               <p className="text-center mb-1">Raúl Sanchez</p>
               <p className="text-center mt-0"><strong>ING. INFORMÁTICA</strong></p>
@@ -28,7 +29,7 @@ function About(props) {
 
             <div className="col-8 offset-2 offset-sm-0 col-sm-4 px-5 px-sm-3">
               <div className="avatar mx-auto mb-2">
-                <img alt="Luis" src={PicLuis} className="img-fluid img-thumbnail rounded-circle" />
+                <img alt="Luis" src={PicLuis} className="letter letterD img-fluid" />
               </div>
               <p className="text-center mb-1">Luis Hernández</p>
               <p className="text-center mt-0"><strong>ING. INFORMÁTICA</strong></p>
@@ -36,7 +37,7 @@ function About(props) {
 
             <div className="col-8 offset-2 offset-sm-0 col-sm-4 px-5 px-sm-3">
               <div className="avatar mx-auto mb-2">
-                <img alt="Diego" src={PicDiego} className="img-fluid img-thumbnail rounded-circle" />
+                <img alt="Diego" src={PicDiego} className="letter letterG img-fluid" />
               </div>
               <p className="text-center mb-1">Diego Ramírez</p>
               <p className="text-center mt-0"><strong>ING. COMPUTACIÓN</strong></p>
