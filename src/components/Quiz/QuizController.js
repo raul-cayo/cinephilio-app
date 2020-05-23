4386eaf7147b5aef846ab1aafb0ccf0fa785a065
Raul Sanchez Cayo <raul.sanchez.cayo@gmail.com>
2020-05-23 13:05:22 -0500

    use original path

--- src/components/Quiz/QuizController.js
+++ src/components/Quiz/QuizController.js
@@ -301,11 +301,7 @@ class QuizController extends React.Component {
                 <div className="col-4 p-0">
                   <img
                     className="img-fluid"
-<<<<<<< HEAD
-                    src={ "http://image.tmdb.org/t/p/w500/" + this.state.recommendation.poster_path }
-=======
-                    src={ "http://image.tmdb.org/t/p/w300_and_h450_bestv2/" + this.state.recommendation.poster_path}
->>>>>>> abc34ff874ba326f83982b295d611fbd1c720a7b
+                    src={ "http://image.tmdb.org/t/p/original/" + this.state.recommendation.poster_path }
                     alt="Movie Poster">
                   </img>
                 </div>
